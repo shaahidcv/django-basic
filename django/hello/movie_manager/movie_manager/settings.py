30874b0de839d10e3d8f0447c0e1ec2ce759d215
shaahid <124174943+shaahidcv@users.noreply.github.com>
2025-02-03 15:52:29 +0530

    Update settings.py

--- django/hello/movie_manager/movie_manager/settings.py
+++ django/hello/movie_manager/movie_manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mcc*yjhok=9$4o9pd&)gc3zu2s(+o)#d58g6)-ebo50+tqko22'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
